736c20f157817e96168e853cf53a72a8b926121a
Andres Matose <andresdmatos@gmail.com>
2021-05-05 16:16:43 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>THIS IS WORKING</h1>
+    </div>
+  );
+}
+
+export default App;
